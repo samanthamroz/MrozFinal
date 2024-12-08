797eb3f995df48cd24a788a35a4a6d603e7a2306
samanthamroz <mrozsamantha@gmail.com>
2024-12-08 14:25:59 -0500

    Fixed Foreign Code Issues in reviews.py

--- api/models/MenuItems/reviews.py
+++ api/models/MenuItems/reviews.py
@@ -12,7 +12,7 @@ class Review(Base):
     __tablename__ = "reviews"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    item_name = Column(Integer, ForeignKey("menu_items.item_name"))
+    item_name = Column(String(100), nullable=True)
     rating = Column(Integer, nullable=False)
     review = Column(String(100), nullable=False)
     customer = relationship("Customer", back_populates="reviews")
